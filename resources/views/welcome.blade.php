90ddf5d3578ae943e35566cdf6a88e4e86dccab2
EC2 Default User <ec2-user@ip-172-31-88-14.ec2.internal>
2021-03-01 15:09:46 +0000

    Laravel応用11

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,15 @@
         </div>
 
     </div>
+    
+    <div class="text-right">
+        
+        @if(Auth::check())
+            {{ Auth::user()->name }}
+        @endif
+        
+    </div>
+    
+    @include('users.users', ['users'=>$users])
 
 @endsection
-
